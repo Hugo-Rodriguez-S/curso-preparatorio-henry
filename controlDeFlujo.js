422c3012acaae1518b4bcf7e1674a333c58948e5
HugoRodriguezS <ugo.rodriguez.s@gmail.com>
2023-03-15 16:02:22 -0500

    first commit

---
+++ controlDeFlujo.js
@@ -0,0 +1,19 @@
+//function viajar (destino) {
+  //  if (destino === "brasil") {
+    //    console.log("gire a la izquierda");
+    //} else if (destino === "argentina") {
+      //  console.log ("gire a la derecha");
+    //} else { 
+  //      console.log ("nos perdimos"); 
+//}
+
+
+
+//viajar ("argentina");
+
+function puedoManejar (edad) {
+    if (edad >= 18) {
+        console.log(true);
+    } else
+        console.log(false);
+}
---
+++ variables.js
@@ -0,0 +1,27 @@
+//String
+
+var bootcamp = 'henry';
+
+//numeros
+
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 11.75;
+
+//booleanos
+
+var estoyEntendiwndo = true;
+
+var estoyAburrido = false;
+
+//undefined
+
+var cajon1;
+
+console.log(cajon1);
+
+//null
+
+var sinNada = null;
